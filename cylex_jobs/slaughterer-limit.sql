7397d2e7e51142634607a612d7ca2b337b068d9f
sakreto <rickyneco@gmail.com>
2021-02-03 19:53:28 -0400

    sql files for limit-weight

---
+++ cylex_jobs/slaughterer-limit.sql
@@ -0,0 +1,4 @@
+INSERT INTO essentialmode.items (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES
+	('alive_chicken', 'Alive chicken', 30, 0, 1),
+	('slaughtered_chicken', 'Slaughtered chicken', 30, 0, 1),
+	('packaged_chicken', 'Packaged chicken', 100, 0, 1);
